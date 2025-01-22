5dd2c2148459b7d678c0c84dba38429aa31230d4
RamdiSalma <133445576+RamdiSalma@users.noreply.github.com>
2025-01-22 22:21:39 +0100

    Create math_operations.py

---
+++ Tp_7/mon_project/src/math_operations.py
@@ -0,0 +1,15 @@
+# math_operations.py
+
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("La division par zéro n'est pas permise.")
+    return a / b
