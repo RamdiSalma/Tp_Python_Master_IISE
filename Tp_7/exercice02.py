ed6087240ba7894e0a06ce10f0fdc775a4e08e8f
RamdiSalma <133445576+RamdiSalma@users.noreply.github.com>
2025-01-22 22:20:24 +0100

    Add files via upload

---
+++ Tp_7/exercice02.py
@@ -0,0 +1,12 @@
+#Exercice 2 : 
+
+import os
+repertoire_courrant = os.getcwd()
+
+from datetime import datetime
+maintenant = datetime.now()
+print(maintenant)
+
+import math
+sqrt = math.sqrt(9)
+print(sqrt)
