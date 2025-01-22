e1ed3145dc5b112c0ccc0ee3a57185452129f602
RamdiSalma <133445576+RamdiSalma@users.noreply.github.com>
2025-01-22 22:22:00 +0100

    Add files via upload

---
+++ Tp_7/mon_project/src/__init__.py
@@ -0,0 +1,3 @@
+#src/__init__.py
+from .math_operations import addition, soustraction, multiplication, division
+from .string_operations import concatener, mettre_en_majuscules
---
+++ Tp_7/mon_project/src/string_operations.py
@@ -0,0 +1,7 @@
+# string_operations.py
+
+def concatener(str1, str2):
+    return str1 + str2
+
+def mettre_en_majuscules(chaine):
+    return chaine.upper()
