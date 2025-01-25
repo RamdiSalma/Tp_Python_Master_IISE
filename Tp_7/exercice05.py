e1605861bf1d08b1044692038d99f97270dc475a
RamdiSalma <133445576+RamdiSalma@users.noreply.github.com>
2025-01-25 18:41:25 +0100

    Update exercice05.py

--- Tp_7/exercice05.py
+++ Tp_7/exercice05.py
@@ -2,16 +2,13 @@ import pandas as pd
 
 # Charger le fichier CSV
 file_path = "employees.csv"
-data = pd.read_csv(file_path)
+donne = pd.read_csv(file_path)
 
 # Afficher les cinq premières lignes du jeu de données
 print("Les cinq premières lignes du fichier :")
-print(data.head())
+print(donne.head())
 
-# Calculer et afficher la moyenne d'un champ numérique (par exemple, l'âge des employés)
-average_age = data["Age"].mean()
+# Calculer et afficher la moyenne d'un champ numérique
+average_age = donne["Age"].mean()
 print(f"\nL'âge moyen des employés est : {average_age:.2f} ans")
 
-# Vous pouvez également afficher une statistique supplémentaire, comme la moyenne des salaires
-average_salary = data["Salary"].mean()
-print(f"Le salaire moyen des employés est : {average_salary:.2f} USD")
